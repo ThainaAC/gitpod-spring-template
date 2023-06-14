c12c2005c53f7d8b64bcea0ae4765ad1b927903a
ThainaAC <alvesthaina568@gmail.com>
2023-06-14 18:00:26 +0000

    salvar

---
+++ app/src/main/java/application/controller/JogoController.java
@@ -0,0 +1,27 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import application.model.JogoRepository;
+
+@Controller
+@RequestMapping("/jogo")
+public class JogoController {
+    @Autowired
+    private JogoRepository JogoRepo;
+
+    @RequestMapping ("/list")
+    public String list (Model model) {
+        model.addAttribute("jogo", JogoRepo.findAll());
+        return "/jogo/list";   
+    }
+    @RequestMapping("/insert")
+    public String insert() {
+        return "/jogo/insert";
+    }
+
+    
+}
---
+++ app/src/main/java/application/model/Jogo.java
@@ -0,0 +1,39 @@
+package application.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "Jogos")
+public class Jogo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titulo;    
+    private int anoDelancamento;
+    public int getAnoDelancamento() {
+        return anoDelancamento;
+    }
+    public void setAnoDelancamento(int anoDelancamento) {
+        this.anoDelancamento = anoDelancamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    
+  public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+} 
